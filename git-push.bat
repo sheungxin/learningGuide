de53282ddda87565291a7d2c9ea7ed69ab6e08f7
常新 <xin.chang@ucarinc.com>
2020-11-17 11:21:53 +0800

    2020/11/17 周二 11:21:53.42

--- git-push.bat
+++ git-push.bat
@@ -1,16 +1,16 @@
-echo "Start submitting code to the local repository"
-echo "The current directory is£º%cd%"
+@echo "Start submitting code to the local repository"
+@echo "The current directory is£º%cd%"
 git add *
-echo;
+@echo;
  
-echo "Commit the changes to the local repository"
+@echo "Commit the changes to the local repository"
 set now=%date% %time%
-echo %now%
+@echo %now%
 git commit -m "%now%"
-echo;
+@echo;
  
-echo "Commit the changes to the remote git server"
+@echo "Commit the changes to the remote git server"
 git push
-echo;
+@echo;
  
-echo "Batch execution complete!"
+@echo "Batch execution complete!"
